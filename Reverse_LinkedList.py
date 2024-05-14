cbc162fe1c90e0a456f9ea0ecef027ae7c946260
Coreharshit <86572653+Coreharshit@users.noreply.github.com>
2024-05-15 02:09:11 +0530

    Update Reverse_LinkedList.py
    output:-
    
    2-->5-->3-->1-->
    1-->3-->5-->2-->

--- Reverse_LinkedList.py
+++ Reverse_LinkedList.py
@@ -1,1 +1,43 @@
+class Node:
+    def __init__(self,data,next):
+        self.data=data
+        self.next=next
 
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def insert(self,val):
+        if self.head is None:
+            self.head=Node(val,None)
+            return
+        itr=self.head
+        while(itr.next):
+            itr=itr.next
+        itr.next=Node(val,None)
+
+    def reverse(self):
+        prev=None
+        Current=self.head
+        while(Current is not None):
+            next=Current.next
+            Current.next=prev
+            prev=Current
+            Current=next
+        self.head=prev
+
+    def printit(self):
+        itr=self.head
+        while(itr):
+            print(itr.data,end="-->")
+            itr=itr.next
+        print()
+if __name__=="__main__":
+    ll=LinkedList()
+    ll.insert(2)
+    ll.insert(5)
+    ll.insert(3)
+    ll.insert(1)
+    ll.printit()
+    ll.reverse()
+    ll.printit()
